0ef7653754d4664d88c7dfd752a7480fccd0fd77
Luis Gerardo Martinez Buentello <lmb@nufin.com.mx>
2026-01-19 18:30:31 +0000

    usa la imagen que esta en imagenes playstorearrow

--- src/components/icons/google-play.tsx
+++ src/components/icons/google-play.tsx
@@ -1,21 +1,9 @@
-export function GooglePlayIcon(props: { className?: string }) {
+export function GooglePlayIcon({ className }: { className?: string }) {
   return (
-    <svg
-      viewBox="0 0 512 512"
-      xmlns="http://www.w3.org/2000/svg"
-      shapeRendering="crispEdges"
-      {...props}
-    >
-      <path fill="#4285F4" d="M325.3 234.3L104.6 13l280.5 161.2-59.8 60.1z" />
-      <path
-        fill="#34A853"
-        d="M47.4 14.5C43.8 20.8 42 29.6 42 39.9v432.2c0 10.3 1.8 19.1 5.4 25.4l254.8-254.9L47.4 14.5z"
-      />
-      <path fill="#EA4335" d="M104.6 499l220.7-220.7 59.8 60.1L104.6 499z" />
-      <path
-        fill="#FBBC04"
-        d="M380.1 278.4l112.5-64.6c27.3-15.6 27.3-41.2 0-56.9l-107.4-61.6-59.8 60.1 54.7 123z"
-      />
-    </svg>
+    <img
+      src="/images/playstorearrow.png"
+      alt="Google Play icon"
+      className={className}
+    />
   );
 }
