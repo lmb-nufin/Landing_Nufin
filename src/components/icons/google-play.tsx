e18ceb16a9b709fbb0daa3e65c0ed271003dd3c5
Luis Gerardo Martinez Buentello <lmb@nufin.com.mx>
2026-01-19 18:23:26 +0000

    el tema es que la flecha de google play esta mal y se ve mal. no top tie

--- src/components/icons/google-play.tsx
+++ src/components/icons/google-play.tsx
@@ -2,11 +2,11 @@ import type { SVGProps } from "react";
 
 export function GooglePlayIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path fill="#4285F4" d="M325.3 234.3L104.6 13l280.5 161.2-59.8 60.1z" />
-      <path fill="#34A853" d="M47.4 14.5C43.8 20.8 42 29.6 42 39.9v432.2c0 10.3 1.8 19.1 5.4 25.4l254.8-254.9L47.4 14.5z" />
-      <path fill="#EA4335" d="M104.6 499l220.7-220.7 59.8 60.1L104.6 499z" />
-      <path fill="#FBBC04" d="M380.1 278.4l112.5-64.6c27.3-15.6 27.3-41.2 0-56.9l-107.4-61.6-59.8 60.1 54.7 123z" />
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M3.66,2.32,19.82,12,3.66,21.68A1.55,1.55,0,0,1,1.5,20.44V3.56A1.55,1.55,0,0,1,3.66,2.32Z" fill="#34A853"/>
+      <path d="M22.5,10.64,5.1,1.06,19.82,12Z" fill="#4285F4"/>
+      <path d="M5.1,22.94l17.4-9.58L19.82,12Z" fill="#EA4335"/>
+      <path d="M22.5,13.36,19.82,12,5.1,22.94Z" fill="#FBBC04"/>
     </svg>
   );
 }
