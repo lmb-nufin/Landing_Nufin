95eb1f4117b93319f435f83923b2b291fd601cf9
Luis Gerardo Martinez Buentello <lmb@nufin.com.mx>
2026-01-19 18:34:23 +0000

    el logo de nufin esta en /home/user/studio/public/images/nufinlogo.jpg

--- src/components/landing/logo.tsx
+++ src/components/landing/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export function Logo({ className }: { className?: string }) {
   return (
     <img
-      src="/images/nufin-logo.png"
+      src="/images/nufinlogo.jpg"
       alt="Nufin Logo"
       className={cn("w-auto object-contain", className)}
     />
