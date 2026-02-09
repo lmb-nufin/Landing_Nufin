1e48f75354ebd7fd195e6944ac03ebe69401d191
Luis Gerardo Martinez Buentello <lmb@nufin.com.mx>
2026-02-09 14:48:01 +0000

    obten el tuyo ahora es un boton a google play igual

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -20,7 +20,8 @@ export function HeroSection() {
           
           <div className={cn(
             "lg:col-span-1 text-center lg:text-left space-y-2 self-center",
-            isClient ? "animate-fade-in" : "opacity-0"
+            "transition-opacity",
+             isClient ? "animate-fade-in" : "opacity-0"
           )}>
             <h1 className="text-3xl md:text-5xl font-display font-black text-gray-900 leading-[1.1]">
               Préstamos<br/>inmediatos,<br/><span className="text-deeppurple">sin historial</span>
@@ -28,14 +29,17 @@ export function HeroSection() {
             <p className="text-sm text-gray-600 font-medium max-w-md mx-auto lg:mx-0 pb-2">
               De <span className="font-bold text-gray-900">$500</span> a <span className="font-bold text-gray-900">$9,000 MXN</span> directos a tu cuenta desde tu celular.
             </p>
-            <Button size="default" className="font-bold text-base px-8">
-              Obtén el tuyo ahora
+            <Button size="default" className="font-bold text-base px-8" asChild>
+              <a href="https://play.google.com/store/apps/details?id=com.nufin.app&hl=en" target="_blank" rel="noopener noreferrer">
+                Obtén el tuyo ahora
+              </a>
             </Button>
           </div>
 
           <div className={cn(
             "lg:col-span-2 relative w-full flex flex-col items-center lg:items-end",
-            isClient ? "animate-fade-in" : "opacity-0"
+            "transition-opacity",
+             isClient ? "animate-fade-in" : "opacity-0"
           )}>
             <div className="relative w-full aspect-[16/9] rounded-3xl overflow-hidden shadow-xl">
               <Image 
