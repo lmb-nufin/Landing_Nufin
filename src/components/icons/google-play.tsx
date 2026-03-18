ff3b2e5597e486bfbd6b803f1a5ab2fb86c3abf3
lmb-nufin <lmb@nufin.com.mx>
2026-03-18 11:51:16 -0600

    Delete src directory

--- src/components/icons/google-play.tsx
+++
@@ -1,9 +0,0 @@
-export function GooglePlayIcon({ className }: { className?: string }) {
-  return (
-    <img
-      src="/images/googlelogo.png"
-      alt="Google Play icon"
-      className={className}
-    />
-  );
-}
